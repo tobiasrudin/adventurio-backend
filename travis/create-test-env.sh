13c42ea4053cbe547b1e645d294f4b256bd71168
Tobias Rudin <t.rudin@gmail.com>
2019-03-13 19:34:42 +0100

    fixed testing table name

--- travis/create-test-env.sh
+++ travis/create-test-env.sh
@@ -2,7 +2,7 @@
 docker network create sam-local
 docker run -d -p 8000:8000 --network lambda-local \
 	--name dynamodb cnadiminti/dynamodb-local
-aws dynamodb create-table --table-name adventurio-games-dev \
+aws dynamodb create-table --table-name adventurio-games-test \
 	--attribute-definitions AttributeName=id,AttributeType=S \
 	--key-schema AttributeName=id,KeyType=HASH \
 	--provisioned-throughput ReadCapacityUnits=5,WriteCapacityUnits=5 \
