7e306d5e7005321d3d057f1366042bffd5af0fa1
Tobias Rudin <t.rudin@gmail.com>
2019-03-14 22:20:57 +0100

    transforming longlat for dynamodb

--- lambda/get-game/index.py
+++ lambda/get-game/index.py
@@ -1,6 +1,8 @@
 from uuid import UUID
 import json
 import dynamodb
+import coordinates
+import uuid_checker
 
 
 def handler(event, context):
@@ -10,7 +12,7 @@ def handler(event, context):
 
 def get_game(gameId):
 
-    if not is_valid_uuid(gameId):
+    if not uuid_checker.is_valid_uuid(gameId):
         return {
             'headers': {
                 'Content-Type': 'application/json'
@@ -30,6 +32,8 @@ def get_game(gameId):
             'body': json.dumps({'message': 'Requested game does not exist'})
         }
 
+    game = coordinates.from_strings_to_numbers(game)
+
     return {
         'headers': {
             'Content-Type': 'application/json'
@@ -37,31 +41,3 @@ def get_game(gameId):
         'statusCode': 200,
         'body': json.dumps(game)
     }
-
-
-def is_valid_uuid(uuid_to_test, version=4):
-    """
-    Check if uuid_to_test is a valid UUID.
-
-    Parameters
-    ----------
-    uuid_to_test : str
-    version : {1, 2, 3, 4}
-
-    Returns
-    -------
-    `True` if uuid_to_test is a valid UUID, otherwise `False`.
-
-    Examples
-    --------
-    >>> is_valid_uuid('c9bf9e57-1685-4c89-bafb-ff5af830be8a')
-    True
-    >>> is_valid_uuid('c9bf9e58')
-    False
-    """
-    try:
-        uuid_obj = UUID(uuid_to_test, version=version)
-    except:
-        return False
-
-    return str(uuid_obj) == uuid_to_test
--- lambda/put-game/index.py
+++ lambda/put-game/index.py
@@ -1,6 +1,8 @@
 from uuid import UUID
 import json
 import dynamodb
+import coordinates
+import uuid_checker
 
 
 def handler(event, context):
@@ -10,7 +12,7 @@ def handler(event, context):
 
 def put_game(game, gameId):
 
-    if not is_valid_uuid(gameId):
+    if not uuid_checker.is_valid_uuid(gameId):
         return {
             'headers': {
                 'Content-Type': 'application/json'
@@ -19,36 +21,9 @@ def put_game(game, gameId):
             "body": json.dumps({"message": "Error: Invalid UUID format"})
         }
 
+    game = coordinates.from_numbers_to_strings(game)
     dynamodb.put_game(game, gameId)
 
     return {
         'statusCode': 201
     }
-
-
-def is_valid_uuid(uuid_to_test, version=4):
-    """
-    Check if uuid_to_test is a valid UUID.
-
-    Parameters
-    ----------
-    uuid_to_test : str
-    version : {1, 2, 3, 4}
-
-    Returns
-    -------
-    `True` if uuid_to_test is a valid UUID, otherwise `False`.
-
-    Examples
-    --------
-    >>> is_valid_uuid('c9bf9e57-1685-4c89-bafb-ff5af830be8a')
-    True
-    >>> is_valid_uuid('c9bf9e58')
-    False
-    """
-    try:
-        uuid_obj = UUID(uuid_to_test, version=version)
-    except:
-        return False
-
-    return str(uuid_obj) == uuid_to_test
